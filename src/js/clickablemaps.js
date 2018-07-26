75cfe9337251feed5aec8ce5ae7d3d6ba97ffa88
MEHRYAR.S <me@mehryars.com>
2018-07-26 18:04:44 +0430

    make it easy to use

--- src/js/clickablemaps.js
+++ src/js/clickablemaps.js
@@ -1,65 +1,67 @@
-(function($) {
+(function ($) {
     $.extend($.fn, {
-        clickablemaps: function(param, options) {
+        clickablemaps: function (param, options) {
             options = $.extend({
-                
+
             }, options);
 
-            $(".map").append("<div class='tooltip__wrapper'></div>");
-            $(".tooltip__wrapper").prepend("<div class='tooltip__text'></div>");
-            $("svg a").each(function(){
+            var map = $(this).html();
+            $(this).html('');
+            $(this).append('<div class="clickablemaps__map"></div>');
+            $(this).append('<div class="info"><div class="info__block__wrapp"></div></div>');
+            $('.clickablemaps__map').html(map);
+            $('.clickablemaps__map').append('<div class="tooltip__wrapper"></div>');
+            $('.tooltip__wrapper').prepend('<div class="tooltip__text"></div>');
+            $('svg a').each(function () {
+                $(this).mouseover(function () {
+                    var tooltipText = $(this).data('tooltip');
 
-                //Tooltip
-                $(this).mouseover(function(){
-                    var tooltip__text = $(this).data("tooltip");
-
-                    $(".tooltip__wrapper").show();
-                    $(".tooltip__text").html(tooltip__text);
+                    $('.tooltip__wrapper').show();
+                    $('.tooltip__text').html(tooltipText);
                 });
 
-                $(this).mouseout(function(){
-                    $(".tooltip__wrapper").hide();
+                $(this).mouseout(function () {
+                    $('.tooltip__wrapper').hide();
                 });
 
-                $(this).mousemove(function(event){
-                    var x__position= (event.clientX) + 'px';
-                    var y__position= (event.clientY - 45) + 'px';
+                $(this).mousemove(function (event) {
+                    var xPosition = (event.clientX) + 'px';
+                    var yPosition = (event.clientY - 45) + 'px';
 
-                    $(".tooltip__text").css({"top": y__position, "left": x__position});
+                    $('.tooltip__text').css({
+                        'top': yPosition,
+                        'left': xPosition
+                    });
                 });
 
-                //Read Data
-                $(this).click(function(event){
+                $(this).click(function (event) {
                     event.preventDefault();
-                    var city__name = $(this).data("id");
+                    var cityName = $(this).data('id');
 
-                    $("svg a path").removeClass("this");
-                    $(this).find("path").addClass("this");
-                    $('.info__block__wrapp').html("");
+                    $('svg a path').removeClass('this');
+                    $(this).find('path').addClass('this');
+                    $('.info__block__wrapp').html('');
 
                     $.ajax({
-                        dataType: "json",
-                        url: "http://www.abanegan.com/demo/json/agents.json",
-                        success: function(response){
-                            var agents__list = [];
-                            response[city__name].agents.map(function (item, index){
-                                agents__list.push(
-                                    "<div class='block'><div class='block__image'><div class='block__image__wrapp'><img src='img/shop.jpg'></div></div><div class='block__information'><div class='block__information__wrapp'><ul><li><h3 class='block__information__title'>" + item.store + "</h3></li><li><p class='block__information__manager'>" + item.manager + "</p></li><li><p class='block__information__address'>" + item.address + "</p></li><li><p class='block__information__phone'>" + item.phone + "</p></li><li><a href='#' class='block__information__email'></a></li></ul></div></div></div>"
+                        dataType: 'json',
+                        url: 'http://www.abanegan.com/demo/json/agents.json',
+                        success: function (response) {
+                            var agentsList = [];
+                            response[cityName].agents.map(function (item, index) {
+                                agentsList.push(
+                                    '<div class="block"><div class="block__image"><div class="block__image__wrapp"><img src="img/shop.jpg"></div></div><div class="block__information"><div class="block__information__wrapp"><ul><li><h3 class="block__information__title">' + item.store + '</h3></li><li><p class="block__information__manager">' + item.manager + '</p></li><li><p class="block__information__address">' + item.address + '</p></li><li><p class="block__information__phone">' + item.phone + '</p></li><li><a href="#" class="block__information__email"></a></li></ul></div></div></div>'
                                 );
                             });
-    
-                            $('.info__block__wrapp').append(agents__list);
-                
+                            $('.info__block__wrapp').append(agentsList);
+                            console.log('test');
                         },
                         statusCode: {
-                            404: function() {
-                            alert('There was a problem with the server.  Try again soon!');
-                            }
+                            404: () => alert('There was a problem with the server.  Try again soon!')
                         }
                     });
                 });
             });
-        
+
             return this;
         }
     });
